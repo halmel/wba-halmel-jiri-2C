5b38d554b7774177be137a548502e1a232885ebb
halmel <j.h.nevim@seznam.cz>
2023-04-26 14:09:39 +0200

    ERWRWERWERWE

--- individual-work/node/server.js
+++ individual-work/node/server.js
@@ -1,7 +1,5 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
-  console.log("zsgfvzusdftzsdfzu")
-  res.write('Hello World!');
-  res.end(); 
-}).listen(8080); 
+while(1) {
+  throw console.error();
+
+}
