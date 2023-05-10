1b73d43a3e12a3b84622a2d618bf38a0b1cdbc35
halmel <j.h.nevim@seznam.cz>
2023-05-10 15:11:38 +0200

    okkk

---
+++ individual-work/js/test.js
@@ -0,0 +1,27 @@
+const internal = require("stream");
+console.log(nasobyt(59,68))
+    console.log(nasobyt(0,12))
+    console.log(dekliifr(54,86))
+    console.log(dekliifr(0,0))
+
+//const carbrands =["Audi","BMW","Mercedes","Volksagn","Vokvo","sodja"]
+//var x=carbrands.length-1;
+//carbrands.forEach(element => {
+//    console.log(carbrands[x])
+//                x--
+//});
+//for (const iterator of carbrands) {
+//    console.log(iterator)
+//}
+function nasobyt(a,b){
+        return a*b
+
+}
+function dekliifr(a,b){
+    if(a!=0 , b!=0){
+        console.log(a/b)
+        return a/b
+    }else{
+        return "erroro"
+    }
+}
